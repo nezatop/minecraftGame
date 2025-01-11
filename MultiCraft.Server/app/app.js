369990a51d8b533e41aecf90fb20dfdd201bd0e6
Никита Королёв <n.korolev.mail@gmail.com>
2025-01-11 18:54:56 +0300

    add SSL

--- MultiCraft.Server/app/app.js
+++ MultiCraft.Server/app/app.js
@@ -1,29 +1,37 @@
-import {WebSocketServer} from 'ws'; // Импортируем WebSocketServer
-
+import { WebSocketServer } from 'ws';
 import express from 'express';
-import http from 'http';
+import https from 'https';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
-import {loadPlayerData, savePlayerData, playerData} from './utils/storage.js';
-import {handleClientMessage, broadcast, SendEntities} from './routes/player.js';
-import {PORT} from './config.js';
-import {clients} from './utils/chunk.js';
-import fs from "fs";
-import path from "path";
+import { loadPlayerData, savePlayerData, playerData } from './utils/storage.js';
+import { handleClientMessage, broadcast, SendEntities } from './routes/player.js';
+import { PORT } from './config.js';
+import { clients } from './utils/chunk.js';
 
 const updateInterval = 10000 / 200;
 
+import { fileURLToPath } from 'url';
+
+// Определяем __filename и __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Чтение сертификата и ключа
+const key = fs.readFileSync(path.join(__dirname, '../certs/key.pem'));
+const cert = fs.readFileSync(path.join(__dirname, '../certs/cert.pem'));
+
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer({ key, cert }, app); // Создаём HTTPS сервер
 
 app.use(cors());
 
-const wss = new WebSocketServer({server}); // Используем WebSocketServer вместо WebSocket.Server
+const wss = new WebSocketServer({ server }); // Используем WebSocketServer с HTTPS сервером
 
 setInterval(SendEntities, updateInterval);
 
 wss.on('connection', (socket) => {
-
     socket.on('message', (message) => {
         try {
             const data = JSON.parse(message);
@@ -43,11 +51,11 @@ wss.on('connection', (socket) => {
                 playerData.delete(clientId);
             }
         }
-        broadcast(JSON.stringify({type: 'player_disconnected', player_id: clientId}));
+        broadcast(JSON.stringify({ type: 'player_disconnected', player_id: clientId }));
         clients.delete(socket);
     });
 });
 
 server.listen(PORT, () => {
-    console.log(`[SERVER] Start on port ${PORT}`);
+    console.log(`[SERVER] Start on port ${PORT} with WSS`);
 });
