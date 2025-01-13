4c3dd4061df6bb35241d0e747509219566c7318b
Никита Королёв <n.korolev.mail@gmail.com>
2025-01-13 13:08:16 +0300

    fix

--- MultiCraft.Server/app/app.js
+++ MultiCraft.Server/app/app.js
@@ -19,8 +19,9 @@ const __dirname = path.dirname(__filename);
 
 const updateInterval = 10000 / 200;
 
-const key = fs.readFileSync(path.join(__dirname, '../certs/key.pem'));
-const cert = fs.readFileSync(path.join(__dirname, '../certs/cert.pem'));
+const key = fs.readFileSync('/etc/letsencrypt/live/bloxter.fun/privkey.pem');
+const cert = fs.readFileSync('/etc/letsencrypt/live/bloxter.fun/cert.pem');
+
 
 console.log("key", key);
 console.log("cert", cert);
